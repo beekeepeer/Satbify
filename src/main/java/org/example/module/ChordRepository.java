d795455af3ee68f95798610bab1b7533eaa65c21
beekeepeer <dmitrygoryuk@gmail.com>
2024-01-01 19:28:07 +0100

    repository skeleton

--- src/main/java/org/example/module/ChordRepository.java
+++ src/main/java/org/example/module/ChordRepository.java
@@ -14,133 +14,18 @@ public class ChordRepository {
     // Close Spacing:
     static {
         chordsRepositoryList.add(new Chord(MelodicPosition.I, Spacing.CLOSE, Inversion.ROOT_POSITION, ChordType.TRIAD, Alteration.NONE, Occurrence.COMMON, new NoteOfScale(Degree.I, 5), new NoteOfScale(Degree.V, 4), new NoteOfScale(Degree.III, 4), new NoteOfScale(Degree.I, 4)));
-
-        // bass octave loser
         chordsRepositoryList.add(new Chord(MelodicPosition.I, Spacing.CLOSE, Inversion.ROOT_POSITION, ChordType.TRIAD, Alteration.NONE, Occurrence.COMMON, new NoteOfScale(Degree.I, 5), new NoteOfScale(Degree.V, 4), new NoteOfScale(Degree.III, 4), new NoteOfScale(Degree.I, 3)));
+        chordsRepositoryList.add(new Chord(MelodicPosition.III, Spacing.CLOSE, Inversion.ROOT_POSITION, ChordType.TRIAD, Alteration.NONE, Occurrence.COMMON, new NoteOfScale(Degree.III, 5), new NoteOfScale(Degree.I, 5), new NoteOfScale(Degree.V, 4), new NoteOfScale(Degree.I, 4)));
+        chordsRepositoryList.add(new Chord(MelodicPosition.III, Spacing.CLOSE, Inversion.ROOT_POSITION, ChordType.TRIAD, Alteration.NONE, Occurrence.COMMON, new NoteOfScale(Degree.III, 5), new NoteOfScale(Degree.I, 5), new NoteOfScale(Degree.V, 4), new NoteOfScale(Degree.I, 3)));
+        chordsRepositoryList.add(new Chord(MelodicPosition.V, Spacing.CLOSE, Inversion.ROOT_POSITION, ChordType.TRIAD, Alteration.NONE, Occurrence.COMMON, new NoteOfScale(Degree.V, 5), new NoteOfScale(Degree.III, 5), new NoteOfScale(Degree.I, 5), new NoteOfScale(Degree.I, 4)));
+        chordsRepositoryList.add(new Chord(MelodicPosition.V, Spacing.CLOSE, Inversion.ROOT_POSITION, ChordType.TRIAD, Alteration.NONE, Occurrence.COMMON, new NoteOfScale(Degree.V, 5), new NoteOfScale(Degree.III, 5), new NoteOfScale(Degree.I, 5), new NoteOfScale(Degree.I, 5)));
 
-        chordsRepositoryList.add(new Chord(
-                MelodicPosition.III,
-                Spacing.CLOSE,
-                Inversion.ROOT_POSITION,
-                ChordType.TRIAD,
-                Alteration.NONE,
-                Occurrence.COMMON,
-                new NoteOfScale(Degree.III, 5),
-                new NoteOfScale(Degree.I, 5),
-                new NoteOfScale(Degree.V, 4),
-                new NoteOfScale(Degree.I, 4)));
-
-        chordsRepositoryList.add(new Chord(         // Bass one octave lower
-                MelodicPosition.III,
-                Spacing.CLOSE,
-                Inversion.ROOT_POSITION,
-                ChordType.TRIAD,
-                Alteration.NONE,
-                Occurrence.COMMON,
-                new NoteOfScale(Degree.III, 5),
-                new NoteOfScale(Degree.I, 5),
-                new NoteOfScale(Degree.V, 4),
-                new NoteOfScale(Degree.I, 3)));
-
-
-        chordsRepositoryList.add(new Chord(
-                MelodicPosition.V,
-                Spacing.CLOSE,
-                Inversion.ROOT_POSITION,
-                ChordType.TRIAD,
-                Alteration.NONE,
-                Occurrence.COMMON,
-                new NoteOfScale(Degree.V, 5),
-                new NoteOfScale(Degree.III, 5),
-                new NoteOfScale(Degree.I, 5),
-                new NoteOfScale(Degree.I, 4)));
-
-        chordsRepositoryList.add(new Chord(
-                MelodicPosition.V,
-                Spacing.CLOSE,
-                Inversion.ROOT_POSITION,
-                ChordType.TRIAD,
-                Alteration.NONE,
-                Occurrence.COMMON,
-                new NoteOfScale(Degree.V, 5),
-                new NoteOfScale(Degree.III, 5),
-                new NoteOfScale(Degree.I, 5),
-                new NoteOfScale(Degree.I, 5)));
-
-
-        // Open Spacing:
-
-        chordsRepositoryList.add(new Chord(
-                MelodicPosition.III,
-                Spacing.OPEN,
-                Inversion.ROOT_POSITION,
-                ChordType.TRIAD,
-                Alteration.NONE,
-                Occurrence.COMMON,
-                new NoteOfScale(Degree.III, 6),
-                new NoteOfScale(Degree.V, 5),
-                new NoteOfScale(Degree.I, 5),
-                new NoteOfScale(Degree.I, 4))); // done
-
-        chordsRepositoryList.add(new Chord(                 // same with bass an octave higher
-                MelodicPosition.III,
-                Spacing.OPEN,
-                Inversion.ROOT_POSITION,
-                ChordType.TRIAD,
-                Alteration.NONE,
-                Occurrence.COMMON,
-                new NoteOfScale(Degree.III, 6),
-                new NoteOfScale(Degree.V, 5),
-                new NoteOfScale(Degree.I, 5),
-                new NoteOfScale(Degree.I, 5)));     // octave lower
-
-        chordsRepositoryList.add(new Chord(
-                MelodicPosition.I,
-                Spacing.OPEN,
-                Inversion.ROOT_POSITION,
-                ChordType.TRIAD,
-                Alteration.NONE,
-                Occurrence.COMMON,
-                new NoteOfScale(Degree.I, 6),
-                new NoteOfScale(Degree.III, 5),
-                new NoteOfScale(Degree.V, 4),
-                new NoteOfScale(Degree.I, 4)));
-
-        chordsRepositoryList.add(new Chord(                 // same with bass an octave lower
-                MelodicPosition.I,
-                Spacing.OPEN,
-                Inversion.ROOT_POSITION,
-                ChordType.TRIAD,
-                Alteration.NONE,
-                Occurrence.COMMON,
-                new NoteOfScale(Degree.I, 6),
-                new NoteOfScale(Degree.III, 5),
-                new NoteOfScale(Degree.V, 4),
-                new NoteOfScale(Degree.I, 3)));
-
-        chordsRepositoryList.add(new Chord(
-                MelodicPosition.I,
-                Spacing.OPEN,
-                Inversion.ROOT_POSITION,
-                ChordType.TRIAD,
-                Alteration.NONE,
-                Occurrence.COMMON,
-                new NoteOfScale(Degree.V, 5),
-                new NoteOfScale(Degree.I, 5),
-                new NoteOfScale(Degree.III, 4),
-                new NoteOfScale(Degree.I, 4)));
-
-        chordsRepositoryList.add(new Chord(                 // same with bass an octave lower
-                MelodicPosition.I,
-                Spacing.OPEN,
-                Inversion.ROOT_POSITION,
-                ChordType.TRIAD,
-                Alteration.NONE,
-                Occurrence.COMMON,
-                new NoteOfScale(Degree.V, 5),
-                new NoteOfScale(Degree.I, 5),
-                new NoteOfScale(Degree.III, 4),
-                new NoteOfScale(Degree.I, 3)));
+        chordsRepositoryList.add(new Chord(MelodicPosition.I, Spacing.OPEN, Inversion.ROOT_POSITION, ChordType.TRIAD, Alteration.NONE, Occurrence.COMMON, new NoteOfScale(Degree.I, 6), new NoteOfScale(Degree.III, 5), new NoteOfScale(Degree.V, 4), new NoteOfScale(Degree.I, 4)));
+        chordsRepositoryList.add(new Chord(MelodicPosition.I, Spacing.OPEN, Inversion.ROOT_POSITION, ChordType.TRIAD, Alteration.NONE, Occurrence.COMMON, new NoteOfScale(Degree.I, 6), new NoteOfScale(Degree.III, 5), new NoteOfScale(Degree.V, 4), new NoteOfScale(Degree.I, 3)));
+        chordsRepositoryList.add(new Chord(MelodicPosition.III, Spacing.OPEN, Inversion.ROOT_POSITION, ChordType.TRIAD, Alteration.NONE, Occurrence.COMMON, new NoteOfScale(Degree.III, 6), new NoteOfScale(Degree.V, 5), new NoteOfScale(Degree.I, 5), new NoteOfScale(Degree.I, 4)));
+        chordsRepositoryList.add(new Chord(MelodicPosition.III, Spacing.OPEN, Inversion.ROOT_POSITION, ChordType.TRIAD, Alteration.NONE, Occurrence.COMMON, new NoteOfScale(Degree.III, 6), new NoteOfScale(Degree.V, 5), new NoteOfScale(Degree.I, 5), new NoteOfScale(Degree.I, 5)));
+        chordsRepositoryList.add(new Chord(MelodicPosition.V, Spacing.OPEN, Inversion.ROOT_POSITION, ChordType.TRIAD, Alteration.NONE, Occurrence.COMMON, new NoteOfScale(Degree.V, 6), new NoteOfScale(Degree.I, 6), new NoteOfScale(Degree.III, 5), new NoteOfScale(Degree.I, 4)));
+        chordsRepositoryList.add(new Chord(MelodicPosition.V, Spacing.OPEN, Inversion.ROOT_POSITION, ChordType.TRIAD, Alteration.NONE, Occurrence.COMMON, new NoteOfScale(Degree.V, 6), new NoteOfScale(Degree.I, 6), new NoteOfScale(Degree.III, 5), new NoteOfScale(Degree.I, 5)));
 
     }
 }
