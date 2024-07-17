feafae85ebb60982e72f0647c1b1fc4961c4842f
Dmitri Goriuc <goriucdmitri@gmail.com>
2024-07-17 20:20:46 +0200

    one public method only

--- src/main/java/com/DAWIntegration/Satbify/Chords.java
+++ src/main/java/com/DAWIntegration/Satbify/Chords.java
@@ -13,7 +13,6 @@ import static com.DAWIntegration.Satbify.repository.ChordRepository.chordsReposi
 public final class Chords {
 
     private static int id;
-
     public static String harmonise(String input) {
         int standard = 68; // todo: set it in applyKeySwitch()
         boolean legato = false; //todo: if true, noteEnd += 20, and join repeated notes.
@@ -236,7 +235,7 @@ public final class Chords {
 
         return x;
     }
-    public static int raiseToDegree(int noteNumber, Degree degree) {
+    private static int raiseToDegree(int noteNumber, Degree degree) {
         var ordinal = degree.ordinal();
         // todo: make a notes list for pentatonic and hexatonic. Add a "Scale" method argument
         List<Integer> notes = List.of(0, 2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19, 21, 23, 24, 26, 28, 29, 31, 33, 35, 36, 38, 40, 41, 43, 45, 47, 48, 50, 52, 53, 55, 57, 59, 60, 62, 64, 65, 67, 69, 71, 72, 74, 76, 77, 79, 81, 83, 84, 86, 88, 89, 91, 93, 95, 96, 98, 100, 101, 103, 105, 107, 108, 110, 112, 113, 115, 117, 119, 120, 122, 124, 125);
