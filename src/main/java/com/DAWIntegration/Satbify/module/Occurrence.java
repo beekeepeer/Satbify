71c9d1f2199d05e37750dd4b04c5e0df865cc551
beekeepeer <108191539+beekeepeer@users.noreply.github.com>
2025-03-24 12:03:09 +0100

    comment

--- src/main/java/com/DAWIntegration/Satbify/module/Occurrence.java
+++ src/main/java/com/DAWIntegration/Satbify/module/Occurrence.java
@@ -2,12 +2,9 @@ package com.DAWIntegration.Satbify.module;
 
 /*
 Value of this enum reflects how often the chord or chord connection is used in music compositional practice.
-
 It should be used for internal purpose of the application but not from an input midi file.
-
 it can influence the priority or order of the output solutions.
-
-it should receive a value only in the ChordRepository database.
+it should receive a value only in the ChordRepository database, or "UNUSABLE" value for future delition.
  */
 public enum Occurrence {
     COMMON,
